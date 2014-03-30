4f6050efa0df26a00226fec3fc1e272d7522d8c9
DavePearce <david.pearce@ecs.vuw.ac.nz>
2014-03-30 19:23:18 +1300

    Plugin system is now opertional, and is almost ready to be used properly.

---
+++ plugins/WhileyBuildSystem/src/wybs/Activator.java
@@ -0,0 +1,22 @@
+package wybs;
+
+/**
+ * The activator for the Whiley Build System plugin. This doesn't really do much!
+ * 
+ * @author David J. Pearce
+ * 
+ */
+public class Activator {
+		
+	public Activator() {
+		
+	}
+	
+	public void start() {
+		System.out.println("WYBS Activator.start() called");
+	}
+	
+	public void stop() {
+		System.out.println("WYBS Activator.stop() called");
+	}
+}
