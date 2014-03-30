dc116773091838802a00c26db48836e4dad76f60
DavePearce <david.pearce@ecs.vuw.ac.nz>
2014-03-30 19:13:36 +1300

    Added Activator for Whiley File System plugin.

---
+++ plugins/WhileyFileSystem/src/wyfs/Activator.java
@@ -0,0 +1,22 @@
+package wyfs;
+
+/**
+ * The activator for the Whiley FIle System plugin. This doesn't really do much!
+ * 
+ * @author David J. Pearce
+ * 
+ */
+public class Activator {
+		
+	public Activator() {
+		
+	}
+	
+	public void start() {
+		System.out.println("WYFS Activator.start() called");
+	}
+	
+	public void stop() {
+		System.out.println("WYFS Activator.stop() called");
+	}
+}
