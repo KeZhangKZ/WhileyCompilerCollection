7516e46554ae1582269d9387e3c0add459d6f92f
DavePearce <david.pearce@ecs.vuw.ac.nz>
2014-04-08 17:39:04 +1200

    Making slow progress on putting together enough infrastructure that this thing can actually work.

--- src/wycc/lang/PluginContext.java
+++ src/wycc/lang/PluginContext.java
@@ -17,4 +17,31 @@ public interface PluginContext {
 	 * @param implementation
 	 */
 	public void register(String extension, Class<?> implementation);
+	
+	/**
+	 * Create a new extension point which subsequent plugins can register
+	 * extensions for.
+	 * 
+	 * @param extension
+	 * @param ep
+	 */
+	public void create(String extension, ExtensionPoint ep);
+	
+	/**
+	 * An extension point in the plugin is a named entity provided by one
+	 * plugin, which other plugins can register extensions for.
+	 * 
+	 * @author David J. Pearce
+	 * 
+	 */
+	public interface ExtensionPoint {
+		
+		/**
+		 * Notify extension point that a new extension has been registered for
+		 * it.
+		 * 
+		 * @param implementation
+		 */
+		public void register(Class<?> implementation);
+	}
 }
