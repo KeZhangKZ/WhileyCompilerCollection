133c7271b67cc4bbb100c18dc64c61f941f59893
DavePearce <david.pearce@ecs.vuw.ac.nz>
2014-05-12 13:01:52 +1200

    Working on the applicatin entry point.

--- src/wycc/Application.java
+++ src/wycc/Application.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import wycc.lang.PluginContext;
 
-public class Application {
+public class Application extends Main {
 	private final PluginContext context;
 	
 	public Application(PluginContext context) {
